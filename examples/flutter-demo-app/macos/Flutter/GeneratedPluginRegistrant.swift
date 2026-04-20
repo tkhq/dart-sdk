e3cf12effea90e0599831493929c233686579f9a
Omkar Shanbhag <omkar@turnkey.io>
2026-04-20 12:34:01 -0400

    codegen fixes

--- examples/flutter-demo-app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/flutter-demo-app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,21 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import device_info_plus
 import flutter_inappwebview_macos
 import flutter_secure_storage_macos
-import path_provider_foundation
+import package_info_plus
+import passkeys_darwin
 import sign_in_with_apple
+import ua_client_hints
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PasskeysPlugin.register(with: registry.registrar(forPlugin: "PasskeysPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  UAClientHintsPlugin.register(with: registry.registrar(forPlugin: "UAClientHintsPlugin"))
 }
--- examples/flutter-demo-app/windows/flutter/generated_plugins.cmake
+++ examples/flutter-demo-app/windows/flutter/generated_plugins.cmake
@@ -6,9 +6,11 @@ list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   flutter_inappwebview_windows
   flutter_secure_storage_windows
+  passkeys_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
